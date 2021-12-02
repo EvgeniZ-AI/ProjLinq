59657bd79b4b31478a4de8359e24a77c045ae537
Женя Вито z z z <evgenizef.ai@gmail.com>
2021-12-03 03:32:56 +1000

    добавил класс класса

---
+++ ProjLinq/Class.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjLinq
+{
+    class Class
+    {
+        public int id { get; set; }
+        public string studentName { get; set; }
+        public int age { get; set; }
+        public int shoolID { get; set; }
+    }
+}
