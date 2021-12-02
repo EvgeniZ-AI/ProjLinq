99c2aa859e26087c639fb5c7d609c0ba532cbe36
Женя Вито z z z <evgenizef.ai@gmail.com>
2021-12-03 03:29:12 +1000

    Добавил класс школы

---
+++ ProjLinq/Shool.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjLinq
+{
+    class Shool
+    {
+        public int id { get; set; }
+        public string NameClass { get; set; }
+        public int quantity { get; set; }
+    }
+}
