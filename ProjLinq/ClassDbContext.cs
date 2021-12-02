0b601fc8873e8d23330b374f4de15e941f4a0d22
Женя Вито z z z <evgenizef.ai@gmail.com>
2021-12-03 03:21:21 +1000

    добавил класс DbConnect

---
+++ ProjLinq/ClassDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace ProjLinq
+{
+    public class ClassDbContext : DbContext
+    {
+        protected ClassDbContext() :base("ShoolDbConnectionsString")
+        {
+        }
+    }
+}
