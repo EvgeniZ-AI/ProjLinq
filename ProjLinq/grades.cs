48de6e679357bb7706d6ce5579dd7509d0aed0c3
Женя Вито z z z <evgenizef.ai@gmail.com>
2021-12-03 18:34:56 +1000

    Добавил класс оценок

---
+++ ProjLinq/grades.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjLinq
+{
+    public class grades
+    {
+        public int id { get; set; }
+        public int аverage { get; set; }
+        public int quantity { get; set; }
+    }
+}
